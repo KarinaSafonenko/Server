823c711fd28bbea81dd553122d1a8ef9a32279b6
Karina <karina.safonenko@gmail.com>
2017-11-14 00:18:54 +0300

    Clients receiving

--- BarebonesServer/main.cpp
+++ BarebonesServer/main.cpp
@@ -36,61 +36,97 @@ void main() {
 	//Сказать винсокету, что сокет слушает
 	listen(listening, SOMAXCONN);
 
-	//Ждем соединения
-	sockaddr_in client;
-	int clientSize = sizeof(client);
-
-	SOCKET clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
-	if (clientSocket == INVALID_SOCKET) {
-		cerr << "Can't create a socket! Quitting" << endl;
-		return;
-	}
-
-	char host[NI_MAXHOST];			//Имя клиента
-	char service[NI_MAXHOST];		//Порт сервера
-
-	ZeroMemory(host, NI_MAXHOST);
-	ZeroMemory(service, NI_MAXSERV);
-
-	if (getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0) {
-		cout << host << " connected on port " << service << endl;
-	}
-	else {
-		inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
-		cout << host << "connected on port " << ntohs(client.sin_port) << endl;
-	}
-
-	//Закрытие сокета
-
-	closesocket(listening);
-
-
-	// Принять и послать сообщение клиенту
-	char buff[4096];
+	fd_set master;
+	FD_ZERO(&master);
+	FD_SET(listening, &master);
 
 	while (true) {
-		ZeroMemory(buff, 4096);
+		fd_set copy = master;
+		int socketCount = select(0, &copy, nullptr, nullptr, nullptr);
+		for (int i = 0; i < socketCount; i++) {
+			SOCKET sock = copy.fd_array[i];
+			if (sock == listening) {
+				SOCKET client = accept(listening, nullptr, nullptr);
+				FD_SET(client, &master);
+				//string wel =  "welcome";
+				//send(client, wel.c_str(), wel.size() + 1, 0);
+			}
+			else {
+				char buff[4096];
+				ZeroMemory(buff, 4096);
+				int bytesIn = recv(sock, buff, 4096, 0);
+				if (bytesIn <= 0) {
+					closesocket(sock);
+					FD_CLR(sock, &master);
+					cout << "Client disconnected" << endl;
 
-		//Ждем сообщения от клиента
-		int bytesReceived = recv(clientSocket, buff, 4096, 0);
+				}
+				else {
+					cout << string(buff, 0, bytesIn) << endl;
+				}
 
-		if (bytesReceived == SOCKET_ERROR) {
-			cerr << "Client disconnected" << endl;
-			break;
+			}
 		}
-
-		if (bytesReceived == 0) {
-			cout << "Client "<<host<<" disconnected" << endl;
-			break;
-		}
-
-		cout << string(buff, 0, bytesReceived) << endl;
-
-		//Отправляем сообщение слиенту
-		send(clientSocket, buff, bytesReceived + 1, 0);
 	}
-	//Закрываем все
-	closesocket(clientSocket);
+	
 	WSACleanup();
 	system("pause");
 }
+
+
+/*
+//Ждем соединения
+sockaddr_in client;
+int clientSize = sizeof(client);
+
+SOCKET clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
+if (clientSocket == INVALID_SOCKET) {
+cerr << "Can't create a socket! Quitting" << endl;
+return;
+}
+
+char host[NI_MAXHOST];			//Имя клиента
+char service[NI_MAXHOST];		//Порт сервера
+
+ZeroMemory(host, NI_MAXHOST);
+ZeroMemory(service, NI_MAXSERV);
+
+if (getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0) {
+cout << host << " connected on port " << service << endl;
+}
+else {
+inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
+cout << host << "connected on port " << ntohs(client.sin_port) << endl;
+}
+
+//Закрытие сокета
+
+closesocket(listening);
+
+
+// Принять и послать сообщение клиенту
+char buff[4096];
+
+while (true) {
+ZeroMemory(buff, 4096);
+
+//Ждем сообщения от клиента
+int bytesReceived = recv(clientSocket, buff, 4096, 0);
+
+if (bytesReceived == SOCKET_ERROR) {
+cerr << "Client disconnected" << endl;
+break;
+}
+
+if (bytesReceived == 0) {
+cout << "Client "<<host<<" disconnected" << endl;
+break;
+}
+
+cout << string(buff, 0, bytesReceived) << endl;
+
+//Отправляем сообщение слиенту
+send(clientSocket, buff, bytesReceived + 1, 0);
+}
+//Закрываем все
+closesocket(clientSocket);*/
